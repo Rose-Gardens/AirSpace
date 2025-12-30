3aed0789bd89a6b3aa8e7c2e16132371dfa25397
Roshin Nishad <roshin006@gmail.com>
2025-12-30 18:54:20 +0400

    Renamed views to state names Part 2

---
+++ AirSpace/UI/OnboardingView/OnboardingSubViews/SetupStartView.swift
@@ -0,0 +1,44 @@
+//
+//  SetupStartView.swift
+//  AirSpace
+//
+//  Created by Hazel Nishad on 9/12/25.
+//
+
+import SwiftUI
+
+// Child View to OnboardingRootView
+struct SetupStartView: View {
+
+  @EnvironmentObject private var airSpace: AirSpaceMananger
+  @EnvironmentObject private var appDelegate: AppDelegate
+
+  var body: some View {
+
+    VStack(alignment: .leading, spacing: 20) {
+      Text(
+        "Navigate to the first desktop space (Desktop 1)."
+      )
+      .fixedSize(horizontal: false, vertical: true)
+      Text(
+        "Press the Start button to begin the setup."
+      )
+      .fixedSize(horizontal: false, vertical: true)
+      Text(
+        "Proceed through each subsequent desktop space in order."
+      )
+      .fixedSize(horizontal: false, vertical: true)
+      Text(
+        "Once you have reached the final desktop space, press the Complete button."
+      )
+      .fixedSize(horizontal: false, vertical: true)
+    }
+    Button("Start") {
+      airSpace.isRecording = true
+      curContent = .recording
+      // Create the first SpaceRecord for the initial Space (no notif yet)
+      appDelegate.spaceChangeHandler()
+    }
+    .padding(.top, 16)
+  }
+}
